4573dfa34df058fc4dbe96f2881d806d80677362
jeybone <jeysonp@gmail.com>
2018-10-24 07:30:13 -0500

    updated javascript to add functions

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -3,20 +3,32 @@ var losses = 0;
 var guessesLeft = 9;
 var userGuess = [];
 var computerChoices = ["a", "b", "c","d", "e", "f","g", "h", "i","j", "k", "l","m", "n", "o","p", "q", "r","s", "t", "u","v", "w", "x","y", "z"];
+var letterToGuess = null; 
 
-var wins = document.getElementById("wins");
-var losses = document.getElementById("losses");
-var guessesLeft = document.getElementById("guessesLeft");
-var yourGuesses = document.getElementById("yourGuesses");
+  var letterChosen = function() {
+    return computerChoices[Math.floor(Math.random() * computerChoices.length)];
+};
+letterToGuess = letterChosen()
+console.log(letterToGuess)
 
 document.onkeyup = function(event) {
-  var userGuess = event.key;
-  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  var userGuess = event.key.toLowerCase(); 
 
-  if (userGuess === computerGuess) {
+console.log(userGuess);
+
+  if (userGuess === letterToGuess) {
     wins++;
-  } else if 
+    guessesLeft = 9;
+    userGuess = [];
+  }
 
+  else if (userGuess != letterToGuess) {
+    guessesLeft --;
+  }
 
-  
-};
+  else if (guessesLeft === 0) {
+    guessesLefts = 9;
+    losses ++;
+    userGuess = [];
+  }
+}
